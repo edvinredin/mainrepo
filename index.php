15e3ae6d813a97eeace3b64f0ffa38b7b5071fb3
Edvin Redin <edvin.redin@gmail.com>
2019-10-30 10:10:31 +0200

    pirmas commit

--- index.php
+++ index.php
@@ -5,5 +5,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-print 'hi';
+<html>
+    <head>
+        <title>Aš valdau HTML!</title> 
+<body style='background-color:<?php printf( "#%06X\n", mt_rand( 0, 0x222222 )); ?>'>
+    </head>
+    <body>
+        <h1><strong>Tomas Steponėnas</strong> - HTML asas</h1>
+        <p>Moku kurti statinį turinį.</p>
+    </body>
+</html>
