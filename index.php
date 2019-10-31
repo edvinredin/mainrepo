9da0a8db257ae3da2860650ffc035d489d79705c
Edvin Redin <edvin.redin@gmail.com>
2019-10-31 10:54:06 +0200

    pirmas commit

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
+  <?php
+   
+  $img_src = 'https://musicglue-images-prod.global.ssl.fastly.net/cypresshill/product/skull-bandana?u=aHR0cHM6Ly9tdXNpY2dsdWUtdXNlci1hcHAtcC0yLXAuczMuYW1hem9uYXdzLmNvbS9vcmlnaW5hbHMvZTlkOGViZTAtNmFlYi0wMTM1LTEzYzAtNDY3OTQxY2RiZjY0&mode=contain&width=700&v=2' ;
+            
+           
+            ?>
+
 <html>
     <head>
-        <title>Aš valdau HTML <?php print ' ir PHP!'; ?>!</title>
     </head>
     <body>
-   <img src="/assets/img/bomb.png/" alt="">
+ <img src="<?php print $img_src;  ?>">
+  <img src="<?php print $img_src;  ?>">
+   <img src="<?php print $img_src;  ?>">
+    <img src="<?php print $img_src;  ?>">
+
     </body>
 </html>
