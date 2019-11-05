e60587fd750ba25dadec766ebbd6968efe312b52
Edvin Redin <edvin.redin@gmail.com>
2019-11-05 10:16:25 +0200

    pirmas commit

--- index.php
+++ index.php
@@ -1,35 +1,21 @@
   <?php
-      $grizai_velai= rand(0,1);
-              $grizai_isgeres= rand(0,1);
+$x= 0       
             ?>
 <html>
     <head>
+        <style>
+        
+        </style>
     </head>
     <body>
-        <h1>Buitine skaiciuokle</h1>
-        <h2>
-            <?php 
-             if ($grizai_velai==1 && $grizai_isgeres==1):
-                  print 'grizai velai ir isgeres ir miegosi ant sofos';
-                      else:
-                  print '';
-              endif;
-                   if ($grizai_velai==0 && $grizai_isgeres==1):
-                  print 'grizai isgeres ir nemiegosi ant sofos';
-                      else:
-                  print '';
-              endif;
-                   if ($grizai_velai==1 && $grizai_isgeres==0):
-                  print 'grizai velai ir nemiegosi ant sofos';
-                      else:
-                  print '';
-              endif;    
-                     if ($grizai_velai==0 && $grizai_isgeres==0):
-                  print 'nieko nepadarei ir nemiegosi ant sofos';
-                      else:
-                  print '';
-              endif;     
+        <h1>
+            <?php
+            for ($x=0; $x<100;$x++){
+                print "Tai yra";
+            print $x;
+             print "-asis ciklas";
+            }
             ?>
-        </h2>
+        </h1>
     </body>
 </html>
